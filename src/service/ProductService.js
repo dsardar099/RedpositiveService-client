ad6e897d9961aac59795022e3a53df7f59ed48e1
Dwaipayan Sardar <dsardar099@gmail.com>
2022-05-08 22:13:42 +0530

    Delete ProductService.js

--- src/service/ProductService.js
+++
@@ -1,8 +0,0 @@
-export class ProductService {
-    getProducts() {
-        return fetch('data/products.json').then(res => {
-            console.log(res)
-            return res.json();
-        }).then(d => d.data);
-    }
-}
